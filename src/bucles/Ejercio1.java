c63de2e6eba8b3b25276da04a3b3eccea7b6d5fb
zul787 <editramirez208@gmail.com>
2026-03-31 15:36:53 -0300

    mi primer commit

---
+++ src/bucles/Ejercio1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bucles;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Ejercio1 {
+    
+}
